9683bf84c4baacee05a8031ca3f63b7267fd7bc2
yeweixian <183383478@qq.com>
2018-04-04 16:29:12 +0800

    加入断路器功能

--- service-ribbon-1/src/main/java/com/danger/study/sr1/service/HomeService.java
+++ service-ribbon-1/src/main/java/com/danger/study/sr1/service/HomeService.java
@@ -1,5 +1,6 @@
 package com.danger.study.sr1.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,12 @@ public class HomeService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "getTimeError")
     public String getTime() {
         return restTemplate.getForObject("http://EUREKA-CLIENT-1/time", String.class);
+    }
+
+    public String getTimeError() {
+        return "Sorry, error!";
     }
 }
