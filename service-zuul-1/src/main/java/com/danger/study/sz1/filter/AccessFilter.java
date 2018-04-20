23a23a90eb16f81757782ccbc8998de8fe323c0f
yeweixian <183383478@qq.com>
2018-04-20 17:07:46 +0800

    加入路由服务filter添加

---
+++ service-zuul-1/src/main/java/com/danger/study/sz1/filter/AccessFilter.java
@@ -0,0 +1,72 @@
+package com.danger.study.sz1.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    /**
+     * 返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下
+     *
+     * @return pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤的顺序
+     *
+     * @return 默认 零
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 这里可以写逻辑判断，是否要过滤，本文true,永远过滤
+     *
+     * @return 默认 false
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问
+     */
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        String accessToken = request.getParameter("token");
+        if (StringUtils.isBlank(accessToken)) {
+            log.warn("token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+                log.error("resp writer error.", e);
+            }
+            return null;
+        }
+        return null;
+    }
+}
