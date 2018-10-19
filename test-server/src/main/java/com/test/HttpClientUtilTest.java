c4d242a98e086c0230bc32902b8b6dd63c1f54c7
yeweixian <183383478@qq.com>
2018-10-19 17:09:24 +0800

    test项目 通过httpclient爬取 https 数据

--- test-server/src/main/java/com/test/HttpClientUtilTest.java
+++ test-server/src/main/java/com/test/HttpClientUtilTest.java
@@ -5,11 +5,22 @@ import com.arronlong.httpclientutil.builder.HCB;
 import com.arronlong.httpclientutil.common.HttpConfig;
 import com.arronlong.httpclientutil.common.SSLs;
 import com.arronlong.httpclientutil.exception.HttpProcessException;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class HttpClientUtilTest {
+
+    private static final Gson gson = new GsonBuilder().serializeNulls().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
+    private static final List<String> professionList = Lists.newArrayList("DRUID", "HUNTER", "MAGE", "PALADIN", "PRIEST", "ROGUE", "SHAMAN", "WARLOCK", "WARRIOR");
 
     public static void main(String[] args) throws HttpProcessException {
         // System.setProperty("javax.net.debug", "all");
@@ -22,6 +33,15 @@ public class HttpClientUtilTest {
                 .url("https://hsreplay.net/analytics/query/list_decks_by_win_rate/")
                 .map(map)
                 .client(HCB.custom().sslpv(SSLs.SSLProtocolVersion.TLSv1_2).build()));
-        System.out.println(result);
+        // System.out.println(result);
+        Optional.ofNullable(gson.fromJson(result, JsonElement.class))
+                .filter(JsonElement::isJsonObject)
+                .map(JsonElement::getAsJsonObject)
+                .map(item -> item.getAsJsonObject("series"))
+                .map(item -> item.getAsJsonObject("data"))
+                .ifPresent(dataJsonObject -> professionList.forEach(item -> {
+                    JsonArray jsonArray = dataJsonObject.getAsJsonArray(item);
+                    System.out.println(jsonArray.size());
+                }));
     }
 }
