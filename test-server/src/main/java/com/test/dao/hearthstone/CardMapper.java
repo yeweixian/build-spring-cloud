c6c549279a1c6ac64cfe0a561e9a8260b2574495
yeweixian <183383478@qq.com>
2018-11-12 18:02:42 +0800

    加入事务注解

--- test-server/src/main/java/com/test/dao/hearthstone/CardMapper.java
+++ test-server/src/main/java/com/test/dao/hearthstone/CardMapper.java
@@ -4,11 +4,15 @@ import com.test.dao.hearthstone.entity.Card;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Mapper
 public interface CardMapper {
 
     @Insert("INSERT INTO cards(id,dbfId,name,cardClass,set,rarity,cost,attack,health,overload,spellDamage,armor,durability,race,type,faction,mechanics,referencedTags,targetingArrowText,playRequirements,entourage,elite,hideStats,classes,multiClassGroup,collectionText,collectible,howToEarn,howToEarnGolden,questReward,artist,flavor,text)" +
             "VALUES (#{card.id},#{card.dbfId},#{card.name},#{card.cardClass},#{card.set},#{card.rarity},#{card.cost},#{card.attack},#{card.health},#{card.overload},#{card.spellDamage},#{card.armor},#{card.durability},#{card.race},#{card.type},#{card.faction},#{card.mechanics},#{card.referencedTags},#{card.targetingArrowText},#{card.playRequirements},#{card.entourage},#{card.elite},#{card.hideStats},#{card.classes},#{card.multiClassGroup},#{card.collectionText},#{card.collectible},#{card.howToEarn},#{card.howToEarnGolden},#{card.questReward},#{card.artist},#{card.flavor},#{card.text})")
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT)
     void addCard(@Param("card") Card card);
 }
