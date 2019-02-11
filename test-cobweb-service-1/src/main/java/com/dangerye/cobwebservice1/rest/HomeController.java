0c075378ff97ab2b5a593965fa5cdec038494609
yeweixian <183383478@qq.com>
2019-02-11 18:01:27 +0800

    cobweb service 1 run hello world

---
+++ test-cobweb-service-1/src/main/java/com/dangerye/cobwebservice1/rest/HomeController.java
@@ -0,0 +1,17 @@
+package com.dangerye.cobwebservice1.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/time")
+    public String getTime() {
+        return "Hello World. I am from port:" + port + ", now time:" + System.currentTimeMillis();
+    }
+}
