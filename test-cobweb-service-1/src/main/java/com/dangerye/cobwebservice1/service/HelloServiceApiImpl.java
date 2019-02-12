d62c5ed08391ba2abe6371c7290ca050f30124ae
yeweixian <183383478@qq.com>
2019-02-12 11:01:50 +0800

    api service impl adjustment

--- test-cobweb-service-1/src/main/java/com/dangerye/cobwebservice1/service/HelloServiceApiImpl.java
+++ test-cobweb-service-1/src/main/java/com/dangerye/cobwebservice1/service/HelloServiceApiImpl.java
@@ -1,6 +1,7 @@
 package com.dangerye.cobwebservice1.service;
 
 import com.dangerye.cobwebservice.api.HelloServiceApi;
+import com.dangerye.cobwebservice.api.response.HelloResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +10,12 @@ public class HelloServiceApiImpl implements HelloServiceApi {
     @Override
     public String sayHello(String name) {
         return "Hello, " + name + "! Nice to meet you! now time:" + System.currentTimeMillis();
+    }
+
+    @Override
+    public HelloResponse getHelloResponse(String name) {
+        HelloResponse result = new HelloResponse();
+        result.setHelloMsg("Hello, " + name + "! Nice to meet you! now time:" + System.currentTimeMillis());
+        return result;
     }
 }
