da054c81c4c4c0fb71a9e4eccc59fdb9aff60055
yeweixian <183383478@qq.com>
2019-02-12 10:33:24 +0800

    create cobweb rest service

---
+++ test-cobweb-rest-service-1/src/main/java/com/dangerye/cobwebrestservice1/CobwebRestService1Application.java
@@ -0,0 +1,12 @@
+package com.dangerye.cobwebrestservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobwebRestService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobwebRestService1Application.class, args);
+    }
+}
