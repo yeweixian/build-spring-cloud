66c88521d312d25cea1ea7039ae853eb75a58914
yeweixian <183383478@qq.com>
2019-02-25 18:44:22 +0800

    CobwebRemoteCallService update

--- test-cobweb/src/main/java/com/dangerye/cobweb/config/CobwebRemoteCallServiceConfiguration.java
+++ test-cobweb/src/main/java/com/dangerye/cobweb/config/CobwebRemoteCallServiceConfiguration.java
@@ -39,29 +39,31 @@ public class CobwebRemoteCallServiceConfiguration {
             log.info("CobwebRemoteCallService start & wait...");
 
             // 如果使用多线程，那就需要线程池，防止并发过高时创建过多线程耗尽资源
-            ExecutorService threadPool = Executors.newFixedThreadPool(100);
+            ExecutorService threadPool1 = Executors.newFixedThreadPool(100);
+            ExecutorService threadPool2 = Executors.newFixedThreadPool(100);
 
             Executors.newSingleThreadExecutor().execute(() -> {
-                try {
-                    while (true) {
+                while (true) {
+                    try {
+                        // 监听端口
                         Socket socketItem = server.accept();
+                        // 执行线程
                         FutureTask<Boolean> futureTask =
                                 new FutureTask<>(() -> CommunicationOperation.handleResponse(socketItem, applicationContext));
-
-                        threadPool.execute(futureTask);
-
-                        boolean needLog = true;
-                        try {
-                            needLog = !futureTask.get(1000, TimeUnit.MILLISECONDS);
-                        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                            futureTask.cancel(true);
-                        }
-                        if (needLog) {
-                            log.error("CobwebRemoteCallService futureTask run fail.");
-                        }
+                        // 控制执行
+                        threadPool1.execute(futureTask);
+                        // 控制超时销毁
+                        threadPool2.execute(() -> {
+                            try {
+                                futureTask.get(1000, TimeUnit.MILLISECONDS);
+                            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                                log.error("CobwebRemoteCallService futureTask run fail.", e);
+                                futureTask.cancel(true);
+                            }
+                        });
+                    } catch (Exception e) {
+                        log.error("CobwebRemoteCallService socket exception", e);
                     }
-                } catch (Exception e) {
-                    log.error("CobwebRemoteCallService socket exception", e);
                 }
             });
         });
