daef29477092c613ff82f183e1db955c9fe32860
yeweixian <183383478@qq.com>
2019-02-26 14:51:03 +0800

    Service operation update

--- test-cobweb/src/main/java/com/dangerye/cobweb/utils/ServiceOperation.java
+++ test-cobweb/src/main/java/com/dangerye/cobweb/utils/ServiceOperation.java
@@ -4,6 +4,7 @@ import com.dangerye.cobweb.entity.TransferResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Slf4j
@@ -20,22 +21,18 @@ public final class ServiceOperation {
         return result;
     }
 
-    public TransferResponse handle(String className, String methodName, Class<?>[] paramTypes, Object[] args) {
-        try {
-            Class<?> beanClass = Class.forName(className);
-            Method method = beanClass.getMethod(methodName, paramTypes);
+    public TransferResponse handle(String className, String methodName, Class<?>[] paramTypes, Object[] args)
+            throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> beanClass = Class.forName(className);
+        Method method = beanClass.getMethod(methodName, paramTypes);
 
-            Object bean = applicationContext.getBean(beanClass);
-            Object result = method.invoke(bean, args);
+        Object bean = applicationContext.getBean(beanClass);
+        Object result = method.invoke(bean, args);
 
-            TransferResponse transferResponse = new TransferResponse();
-            transferResponse.setReturnType(method.getReturnType().getName());
-            transferResponse.setReturnValue(result);
-            return transferResponse;
-        } catch (Exception e) {
-            log.error("ServiceOperation handle error.", e);
-            return null;
-        }
+        TransferResponse transferResponse = new TransferResponse();
+        transferResponse.setReturnType(method.getReturnType().getName());
+        transferResponse.setReturnValue(result);
+        return transferResponse;
     }
 
     private void setApplicationContext(ApplicationContext applicationContext) {
